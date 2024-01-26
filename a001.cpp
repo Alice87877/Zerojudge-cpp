802fbcf7e8fa61b520714aad4fb29e34b826460d
Alice87877 <99130819+Alice87877@users.noreply.github.com>
2024-01-26 23:25:07 +0800

    Add files via upload

---
+++ a001.cpp
@@ -0,0 +1,13 @@
+//https://zerojudge.tw/ShowProblem?problemid=a001
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string a;
+    cin>>a;
+    cout<<"hello, "<<a;
+
+    return 0;
+}
