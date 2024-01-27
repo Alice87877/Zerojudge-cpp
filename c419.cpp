0802e8cdc356d2a82ff1c43077c39f0d30455dd8
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-01-27 21:30:59 +0800

    Add files via upload

---
+++ c419.cpp
@@ -0,0 +1,21 @@
+//https://zerojudge.tw/ShowProblem?problemid=c419
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                cout<<"_";
+            }
+            for(int j=1;j<=i;j++){
+                cout<<"*";
+            }
+        cout<<"\n";
+    }
+
+
+    return 0;
+}
