bc064b0e94b82c84ffc54cd4726700d87439ec36
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-01-27 16:57:19 +0800

    Add files via upload

---
+++ b970.cpp
@@ -0,0 +1,14 @@
+//https://zerojudge.tw/ShowProblem?problemid=b970
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n,i;
+    cin>>n;
+    for(i=1;i<=n;i++)
+        cout<<i<<". I don't say swear words!\n";
+
+    return 0;
+}
