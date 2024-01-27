8145a18b65ed8ac93b56e1bc17241d1ee00a4276
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-01-27 16:53:29 +0800

    Add files via upload

---
+++ d498.cpp
@@ -0,0 +1,14 @@
+//https://zerojudge.tw/ShowProblem?problemid=d498
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n,i;
+    cin>>n;
+    for(i=0;i<n;i++)
+        cout<<"I don't say swear words!\n";
+
+    return 0;
+}
