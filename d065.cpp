741602d3e39fcbf24c16f1fcc35f2a5697f0ab92
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-02-20 00:06:12 +0800

    Add files via upload

---
+++ d065.cpp
@@ -0,0 +1,15 @@
+//https://zerojudge.tw/ShowProblem?problemid=d065
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,max=0;
+    for(int i=0;i<3;i++){
+        cin>>n;
+        if(n>max) max=n;
+    }
+    cout<<max;
+
+    return 0;
+}
