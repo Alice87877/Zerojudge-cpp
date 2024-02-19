fbeff806943ca15517dd09f45e2961d48b9e6710
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-02-20 00:03:07 +0800

    Add files via upload

---
+++ d064.cpp
@@ -0,0 +1,13 @@
+//https://zerojudge.tw/ShowProblem?problemid=d064
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0) cout<<"Even";
+    else cout<<"Odd";
+
+    return 0;
+}
