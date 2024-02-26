4128264d8079fbf28d851a3e21feb4e84a2247e5
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-02-26 10:41:15 +0800

    Add files via upload

---
+++ d877.cpp
@@ -0,0 +1,11 @@
+//https://zerojudge.tw/ShowProblem?problemid=b877
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<(100+b-a)%100;
+    return 0;
+}
