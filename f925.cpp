1a4f268284eef283461bfe7b8c7aaae230882594
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-03-02 11:26:14 +0800

    Add files via upload

---
+++ f925.cpp
@@ -0,0 +1,15 @@
+//https://zerojudge.tw/ShowProblem?problemid=f925
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a==6 && b==12)
+        cout<<"Yes";
+    else
+        cout<<"No";
+
+    return 0;
+}
