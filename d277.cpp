15f3755bd8d58c65c02e8b8fdbbac844013f1c0a
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-03-02 15:26:26 +0800

    Add files via upload

---
+++ d277.cpp
@@ -0,0 +1,11 @@
+//https://zerojudge.tw/ShowProblem?problemid=d277
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    while(cin>>n)
+    cout<<n-(n%2)<<'\n';
+    return 0;
+}
+
