a7642fc5762fda1f2b4bbc0f625da8c690ccf782
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-03-02 11:06:47 +0800

    Add files via upload

---
+++ c185.cpp
@@ -0,0 +1,13 @@
+//https://zerojudge.tw/ShowProblem?problemid=c185
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string a;
+    getline(cin,a);
+    cout<<"Hey "<<a;
+
+    return 0;
+}
