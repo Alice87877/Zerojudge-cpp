c4eaed7d4da1d7b8ebca771d65ba29c2457d760a
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-03-02 11:21:22 +0800

    Add files via upload

---
+++ f700.cpp
@@ -0,0 +1,15 @@
+//https://zerojudge.tw/ShowProblem?problemid=f700
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+   string s;
+   getline(cin,s);
+   cout<<s<<"\n";
+   for(int i=0;s[i];i++)
+    cout<<"~";
+
+    return 0;
+}
